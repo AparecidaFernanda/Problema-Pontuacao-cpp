90c0ecff060fbb0776992b620e6b51f56ebeaaf9
Aparecida Fernanda <aparecidafernandafroes123@gmail.com>
2020-05-19 08:35:06 -0300

    Leitura dos outros dados

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main()
 {
     int compras, volumedc, inadim, atraso, form, total;
     double ticket;
-    string D, C, B;
+    string Pagamento;
     cout << "SISTEMA DE PERFIL DE CLIENTE" << endl;
     cout << "------------------------------------------------------" << endl;
     cout << "INFORME SEUS DADOS DO ULTIMO ANO" << endl;
@@ -14,8 +14,11 @@ int main()
     cout << "Quantas compras o cliente fez no ultimo ano?";
     cin >> compras;
     cout << "Qual o ticket medio?";
-    cin >> ticket
+    cin >> ticket;
 
+    cout << "Quantas vezes o cliente atrasou o pagamento?";
+    cin >> atraso;
+    cout << "A maioria das compras foi em dinheiro, cartao ou boleto (D/C/B)?";
+    cin >> Pagamento;
 
-    if(compras)
 }
